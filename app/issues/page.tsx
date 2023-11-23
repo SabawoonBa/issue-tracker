3ebbec32a8ff89e07f9601d5fa5268678257d023
SabawoonBa <barekzai.sabawoon@afghanhire.org>
2023-11-23 10:00:48 +0430

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Button } from "@radix-ui/themes";
+import React from "react";
 
 const IssuesPage = () => {
   return (
     <div>
-      This page is to show the issues
+      <Button>Add new Issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
