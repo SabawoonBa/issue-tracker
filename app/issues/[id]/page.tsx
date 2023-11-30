9dae533fdd39f0272dafd383224ef01a84c96c2d
SabawoonBa <barekzai.sabawoon@afghanhire.org>
2023-11-30 10:36:16 +0430

    added issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from '@/components/ui/IssueStatusBadge';
 import prisma from '@/prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
@@ -6,16 +8,23 @@ interface Props {
     params: { id: string }
 }
 const IssueDetailsPage = async ({ params }: Props) => {
-    if(typeof params.id !== 'number') notFound();
+    // if(typeof params.id !== 'number') notFound();
 
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
     });
 
-    if( !issue) notFound();
+    if(!issue) notFound();
+
     return (
         <div>
-        <p>{issue?.title}</p>
+            <Heading>{issue.title}</Heading>
+            <Flex gap="2" my='3'>
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+
+            <Card>{issue.description}</Card>
         </div>
     )
     }
